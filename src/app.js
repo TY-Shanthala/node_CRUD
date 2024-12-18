d147ecfba5f419b22260e93b65f126d14f7ca656
TY-Shanthala_Sajjan <shanthalasajjan1998@gmail.com>
2024-12-18 18:09:46 +0530

    cors config for localhost3000

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-const express = require("express")
+const express = require("express");
+const cors = require("cors");
 const userRouter = require("./routes/userRoute");
-const authRoute = require("./routes/authRoute.js")
 const { dbConnect } = require("./config/mongoConnection");
 const cookieParcer = require("cookie-parser");
-const { userAuth } = require("./middleware/auth.js");
-
+const authRoute = require("./routes/authRoute");
 // creation of server
 const app = express();
 app.use(express.json());
-app.use(cookieParcer())
-
+app.use(cookieParcer());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Update with your frontend origin if different
+    credentials: true,
+  })
+);
 
 // routes
 app.use("/", authRoute);
 app.use("/", userRouter);
 
-
 // connecting to the DB and listening to the port
 dbConnect()
-    .then(() => {
-        console.log("successfully connected to db...")
-        app.listen("7777", () => {
-            console.log("port 7777 is up and running...")
-        })
-    })
-    .catch((err) => {
-        console.log(err, "error in the connection")
-    })
+  .then(() => {
+    console.log("successfully connected to db...");
+    app.listen("7777", () => {
+      console.log("port 7777 is up and running...");
+    });
+  })
+  .catch((err) => {
+    console.log(err, "error in the connection");
+  });
