31b3f9b5609ff5fe3237b95b08b9c224345e8115
TY-Shanthala_Sajjan <shanthalasajjan1998@gmail.com>
2025-01-31 12:20:05 +0530

    get all users API

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,70 +5,81 @@ const { userAuth } = require("../middleware/auth");
 
 const userRouter = express.Router();
 userRouter.post("/user/createUser", async (req, resp) => {
-    try {
-        // destructur the user data from request body
-        const { firstName, lastName, age, gmail, password } = req.body
-        // hashing the password
-        const hashPassword = await bcrypt.hash(password, 10);
-        // create the actual object through the user schema
-        const userData = new User({
-            firstName,
-            lastName,
-            age,
-            gmail,
-            password: hashPassword
-        })
-        // save the actual object to the db
-        await userData.save();
-        // send the resp after saving the data to DB
-        resp.json({ message: "user data saved successfully" })
-
-    } catch (err) {
-        resp.send("something went wrong while saving user data")
-    }
-})
+  try {
+    // destructur the user data from request body
+    const { firstName, lastName, age, gmail, password } = req.body;
+    // hashing the password
+    const hashPassword = await bcrypt.hash(password, 10);
+    // create the actual object through the user schema
+    const userData = new User({
+      firstName,
+      lastName,
+      age,
+      gmail,
+      password: hashPassword,
+    });
+    // save the actual object to the db
+    await userData.save();
+    // send the resp after saving the data to DB
+    resp.json({ message: "user data saved successfully" });
+  } catch (err) {
+    resp.send("something went wrong while saving user data");
+  }
+});
 
 // api to get the data from user schema
 userRouter.get("/user/getUserData", userAuth, async (req, resp) => {
-    try {
-        resp.json({
-            data: req.user,
-            message: "data fetched successfully"
-        })
-    } catch (err) {
-        resp.status(400).send("something went wrong while data fetch")
-    }
-
-})
+  try {
+    resp.json({
+      data: req.user,
+      message: "data fetched successfully",
+    });
+  } catch (err) {
+    resp.status(400).send("something went wrong while data fetch");
+  }
+});
 
 // api to delete the user in DB
 userRouter.delete("/user/deleteUser", userAuth, async (req, resp) => {
-    try {
-        const userData = req.user
-        const userId = req.user._id
-        if (userId) {
-            await User.findByIdAndDelete({ _id: userId })
-        } else {
-            throw new Error("Id not found")
-        }
-        resp.json({ message: `${userData.firstName} user deleted successfully` })
-    } catch (err) {
-        resp.status(400).send("something went wrong while deleting user")
+  try {
+    const userData = req.user;
+    const userId = req.user._id;
+    if (userId) {
+      await User.findByIdAndDelete({ _id: userId });
+    } else {
+      throw new Error("Id not found");
     }
-})
+    resp.json({ message: `${userData.firstName} user deleted successfully` });
+  } catch (err) {
+    resp.status(400).send("something went wrong while deleting user");
+  }
+});
+
+// api for getting all users data
+userRouter.get("/user/getAllUsers", userAuth, async (req, resp) => {
+  try {
+    const allUserData = await User.find();
+    resp.json({
+      data: allUserData,
+      message: "fetch success",
+    });
+  } catch {
+    resp.status(400).send("Something went wrong while getting users Data");
+  }
+});
 
 // api to update the user Data
 userRouter.patch("/user/updateUser", userAuth, async (req, resp) => {
-    try {
-        const actualuserData = req.user
-        const newData = req.body;
-        await User.findByIdAndUpdate({ _id: actualuserData._id },newData)
-        resp.json({ message: `${actualuserData.firstName}'s data has updated successfully` })
-    } catch {
-        resp.status(400).send("something went wrong while updating user")
-    }
-})
+  try {
+    const actualuserData = req.user;
+    const newData = req.body;
+    await User.findByIdAndUpdate({ _id: actualuserData._id }, newData);
+    resp.json({
+      message: `${actualuserData.firstName}'s data has updated successfully`,
+    });
+  } catch {
+    resp.status(400).send("something went wrong while updating user");
+  }
+});
 
-
-
-module.exports = userRouter
+module.exports = userRouter;
